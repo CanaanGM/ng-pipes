6dd0518664c19230700d06eead7b6e8fc2705de4
CanaanGM <reakak1@gmail.com>
2023-05-01 22:09:18 +0300

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+
+  date:string = '';
+  amount: number = 0;
+  height: number = 0;
+
+  miles : number = 0;
+
+  Human = { // can be displayed : {{Human | json}}, in the template
+    Str: 10,
+    Int: 10,
+    HP: 100,
+    Agil : 10,
+    Tech: 10
+  }
+
+  onNameChange(val: string){
+    this.name = val;
+  }
+
+  onDateChange(val:string){
+    this.date  = val;
+  }
+
+  onAmountChange(val: string){
+    this.amount = parseFloat( val);
+  }
+  onHeightChange(val: string){
+    this.height = parseFloat( val);
+  }
+
+  onMilesChange(val:string){
+    this.miles = parseFloat(val);
+  }
 }
